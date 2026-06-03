2f2d2ce18d575dc0e26107973b7ec3ba61dd264f
Himanshu saini <himanshusaini@Himanshus-MacBook-Air.local>
2026-06-03 22:07:23 +0530

    client

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const cors = require("cors");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+//Routes
+app.use("/api/auth", require("./routes/authRoutes"));
+
+app.get("/", (req, res) => {
+  res.send("Auth API Running");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
